4793699bd5267b078cd3690d854d1c1a60e45430
nairut <turian@hotmail.com>
2023-07-20 14:49:59 -0300

    Novo envio

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
 import { Celular } from "./screens/Celular";
 import { DesktopForm } from "./screens/DesktopForm";
 import { CellForm } from "./screens/CellForm";
 import { Index } from "./screens/Index";
 
-const App = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <Celular />,
+  },
+  {
+    path: "/celular",
+    element: <Celular />,
+  },
+  {
+    path: "/desktop-form",
+    element: <DesktopForm />,
+  },
+  {
+    path: "/cell-form",
+    element: <CellForm />,
+  },
+  {
+    path: "/index",
+    element: <Index />,
+  },
+]);
 
-  const renderForm = () => {
-    if (isMobile) {
-      return <Celular />;
-    } else {
-      return <DesktopForm />;
-    }
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/*",
-      element: <Index />,
-    },
-    {
-      path: "/form",
-      element: renderForm(),
-    },
-    {
-      path: "/cell-form",
-      element: <CellForm />,
-    },
-    {
-      path: "/index",
-      element: <Index />,
-    },
-  ]);
-
+export const App = () => {
   return <RouterProvider router={router} />;
 };
-
-export default App;
